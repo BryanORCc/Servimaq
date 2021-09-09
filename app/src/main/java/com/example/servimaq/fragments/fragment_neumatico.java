d317e7a62d48ea54ce36d9c6a2126ed4e5c21a1f
Bryan Rodriguez <bryanorcc@outlook.com>
2021-09-08 23:18:42 -0500

    Registro Vehiculos

---
+++ app/src/main/java/com/example/servimaq/fragments/fragment_neumatico.java
@@ -0,0 +1,71 @@
+package com.example.servimaq.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.servimaq.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment_neumatico#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_neumatico extends Fragment {
+
+    View vista;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public fragment_neumatico() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragment_neumatico.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragment_neumatico newInstance(String param1, String param2) {
+        fragment_neumatico fragment = new fragment_neumatico();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        vista = inflater.inflate(R.layout.fragment_neumatico, container, false);
+
+
+        return vista;
+    }
+}
