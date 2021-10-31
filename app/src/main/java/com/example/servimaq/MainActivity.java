c8ec9cd939753d355f8d6c1a815d948e244ffe18
BryanORCc <bryanorcc@outlook.com>
2021-10-30 21:47:40 -0500

    Cambio de DB SQL Server local a PostgreSQL en Heroku

--- app/src/main/java/com/example/servimaq/MainActivity.java
+++ app/src/main/java/com/example/servimaq/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,14 +18,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.example.servimaq.db.SQLConexion;
+import com.itextpdf.text.pdf.codec.JBIG2SegmentReader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etUsuario, etContra;
     Button btnIngresar;
-
-    String mensajeToast = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +53,80 @@ public class MainActivity extends AppCompatActivity {
                 String usuario = etUsuario.getText().toString(),
                         contra = etContra.getText().toString();
 
-                SQLConexion db = new SQLConexion();
-                if(db.Validar(getBaseContext(),usuario,contra)==true){
-                    Intent i = new Intent(MainActivity.this,menu_opciones.class);
-                    finish();
-                    overridePendingTransition(0, 0);
-                    startActivity(i);
-                    overridePendingTransition(0, 0);
+                if(ValidarCampos()){
+                    AndroidNetworking.initialize(getApplicationContext());
+                    Map<String,String> datosCuenta = new HashMap<>();
+                    datosCuenta.put("Usuario",usuario);
+                    datosCuenta.put("Contraseña",contra);
+
+                    JSONObject datosJSON = new JSONObject(datosCuenta);
+                    AndroidNetworking.post("https://whispering-sea-93962.herokuapp.com/T_Cuenta_POST_ALL.php")
+                            .addJSONObjectBody(datosJSON)
+                            .setPriority(Priority.MEDIUM)
+                            .build()
+                            .getAsJSONObject(new JSONObjectRequestListener() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        String validarDatos = response.getString("data");
+                                        Log.e("respuesta: ",""+validarDatos);
+
+                                        //--VALIDAR LOGIN------------------------------------------------------------------------------------------------
+                                        if(!validarDatos.equals("[]")){
+                                            Toast toast = Toast.makeText(getApplicationContext(),"Bienvenido", Toast.LENGTH_SHORT);
+                                            View vista = toast.getView();
+                                            vista.setBackgroundResource(R.drawable.estilo_color_x);
+                                            toast.setGravity(Gravity.CENTER,0,0);
+                                            TextView text = (TextView) vista.findViewById(android.R.id.message);
+                                            text.setTextColor(Color.parseColor("#FFFFFF"));
+                                            text.setTextSize(20);
+                                            toast.show();
+
+                                            Intent i = new Intent(MainActivity.this,menu_opciones.class);
+                                            finish();
+                                            overridePendingTransition(0, 0);
+                                            startActivity(i);
+                                            overridePendingTransition(0, 0);
+
+                                        }else{
+                                            Toast toast = Toast.makeText(getApplicationContext(),"Usuario o Contraseña incorrecto", Toast.LENGTH_SHORT);
+                                            View vista = toast.getView();
+                                            vista.setBackgroundResource(R.drawable.estilo_color_x);
+                                            toast.setGravity(Gravity.CENTER,0,0);
+                                            TextView text = (TextView) vista.findViewById(android.R.id.message);
+                                            text.setTextColor(Color.parseColor("#FFFFFF"));
+                                            text.setTextSize(20);
+                                            toast.show();
+                                        }
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onError(ANError anError) {
+                                    Toast.makeText(getApplicationContext(),"Error:" + anError.getErrorDetail(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"Debe llenar todos los campos", Toast.LENGTH_SHORT);
+                    View vista = toast.getView();
+                    vista.setBackgroundResource(R.drawable.estilo_color_x);
+                    toast.setGravity(Gravity.CENTER,0,0);
+                    TextView text = (TextView) vista.findViewById(android.R.id.message);
+                    text.setTextColor(Color.parseColor("#FFFFFF"));
+                    text.setTextSize(15);
+                    toast.show();
                 }
-                
+
+
             }
         });
+    }
+
+    public boolean ValidarCampos(){
+        return !etUsuario.getText().toString().trim().isEmpty() && !etContra.getText().toString().trim().isEmpty();
     }
 
 }
--- app/src/main/java/com/example/servimaq/db/SQLConexion.java
+++ app/src/main/java/com/example/servimaq/db/SQLConexion.java
@@ -5,24 +5,38 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.StrictMode;
 import android.os.StrictMode.ThreadPolicy;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.example.servimaq.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SQLConexion {
 
+    boolean estado = false;
+
     public Connection ConexionDB(Context c){
 
         Connection conexion=null;
-        try{
+        /*try{
             StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
@@ -30,44 +44,11 @@ public class SQLConexion {
             //conexion= DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.18;databaseName=Servimaq;user=sa;password=123;");
         }catch (Exception e){
             Toast.makeText(c,"Falla de conexion",Toast.LENGTH_SHORT).show();
-        }
+        }*/
         return conexion;
     }
 
-    //--VALIDAR LOGIN--------------------------------------------------------
-    public boolean Validar(Context c, String usuario, String contra){
-        boolean estado = false;
-        try {
-            Statement st = ConexionDB(c).createStatement();
-            ResultSet rs = st.executeQuery("select * from Cuenta where Usuario='"+usuario+"' and Contraseña='"+contra+"';");
-            if(rs.next()){
-                estado = true;
-                Toast toast = Toast.makeText(c,"Bienvenido", Toast.LENGTH_SHORT);
-                View vista = toast.getView();
-                vista.setBackgroundResource(R.drawable.estilo_color_x);
-                toast.setGravity(Gravity.CENTER,0,0);
-                TextView text = (TextView) vista.findViewById(android.R.id.message);
-                text.setTextColor(Color.parseColor("#FFFFFF"));
-                text.setTextSize(20);
-                toast.show();
-            }else{
-                Toast toast = Toast.makeText(c,"Usuario o Contraseña incorrecto", Toast.LENGTH_SHORT);
-                View vista = toast.getView();
-                vista.setBackgroundResource(R.drawable.estilo_color_x);
-                toast.setGravity(Gravity.CENTER,0,0);
-                TextView text = (TextView) vista.findViewById(android.R.id.message);
-                text.setTextColor(Color.parseColor("#FFFFFF"));
-                text.setTextSize(20);
-                toast.show();
-                estado = false;
-            }
-            rs.close();
 
-        } catch (Exception e) {
-            Toast.makeText(c,e.getMessage(),Toast.LENGTH_SHORT).show();
-        }
-        return estado;
-    }
 
     //--REGISTRO VEHICULO--------------------------------------------------------
     public void RegistroVehiculo(Context c, String TipoVehiculo, String FotoVehiculo, String MarcaVehiculo, String ModeloVehiculo){
