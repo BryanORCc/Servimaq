4fccd1dc405899ae4e749b2f355c6bebcc01a2df
BryanORCc <bryanorcc@outlook.com>
2021-11-06 09:34:53 -0500

    cambio

--- app/src/main/java/com/example/servimaq/op_documentos/Documentos.java
+++ app/src/main/java/com/example/servimaq/op_documentos/Documentos.java
@@ -23,6 +23,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.example.servimaq.R;
 import com.example.servimaq.db.SQLConexion;
 import com.example.servimaq.db.TemplatePDF;
@@ -30,11 +34,17 @@ import com.example.servimaq.db.items_lista;
 import com.example.servimaq.menu_opciones;
 import com.example.servimaq.op_catalogo.Catalogo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Documentos extends AppCompatActivity {
 
@@ -79,31 +89,57 @@ public class Documentos extends AppCompatActivity {
         btnGenerarPdf = findViewById(R.id.btnGenerarPdf);
         btnAbrirPdf = findViewById(R.id.btnAbrirPdf);
 
+        //INICIALIZAR CONEXION CON EL SERVICIO WEB*************************
+        AndroidNetworking.initialize(getApplicationContext());
+
         //HABILITAR BOTON - ATRAS - EN LA BARRA DE NAVEGACION************
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         //SELECCIONAR CODIGO CON PRODUCTOS DE SALIDA**************************************************
-        SQLConexion db = new SQLConexion();
-        try {
-            Statement st = db.ConexionDB(getApplicationContext()).createStatement();
-            ResultSet rs = st.executeQuery("select distinct(P.codPedido) from T_Listado L inner join T_Pedido P on L.codPedido = P.codPedido;");
+        AndroidNetworking.post("https://whispering-sea-93962.herokuapp.com/Documentos_POST_SELECT_INNER.php")
+                .setPriority(Priority.IMMEDIATE)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
 
-            if (!rs.next()) {
-                Toast.makeText(getApplicationContext(),"No se encontraron registros",Toast.LENGTH_SHORT).show();
-            }else {
-                do {
-                    listaCodigoPedido.add(rs.getString(1));
-                } while (rs.next());///va agregando cada ID
-            }
-            rs.close();
+                        try {
+                            String validarDatos = response.getString("data");
+                            int contar = 1;
 
-            ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,listaCodigoPedido);
-            spncodPedido.setAdapter(adapter);
+                            if (validarDatos.equals("[]")) {
+                                Toast.makeText(getApplicationContext(), "No se encontraron registros", Toast.LENGTH_SHORT).show();
+                            } else {
 
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-        }//FIN SELECT--------------------------------------------------------------------------
+                                JSONArray array = response.getJSONArray("data");
+                                do {
+                                    JSONObject object = array.getJSONObject(contar - 1);
+                                    listaCodigoPedido.add(object.getString("codpedido"));
+                                    contar++;
+                                } while (contar <= array.length());
+
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                        try {
+                            ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,listaCodigoPedido);
+                            spncodPedido.setAdapter(adapter);
+                        }catch (Exception e){
+                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+
+                });//FIN SELECT--------------------------------------------------------------------------
+
 
 
         //SELECCION DE OPCION EN SPINNER*******************************************************
@@ -124,40 +160,60 @@ public class Documentos extends AppCompatActivity {
                 templatePDF.AddMetaData("Almacen","Salida de Neumaticos", "Servimaq S.A.C."); //-----------------
 
                 //SELECCIONAR INFORMACION PARA EL PDF**************************************************
-                try {
-                    Statement st = db.ConexionDB(getApplicationContext()).createStatement();
-                    ResultSet rs = st.executeQuery("select LS.ItemId, LL.LlantaId, LS.Cantidad, DL.NombreMarca+' - '+V.TipoVehiculo+' - IC:'+CONVERT(varchar(15)," +
-                            "DL.IndiceCarga)+' - IV:'+DL.IndiceVelocidad+' - '+DL.Construccion+' - PM:'+CONVERT(varchar(15),DL.PresionMaxima)+' - CLASI:'+DL.Clasificacion" +
-                            "+' - ANC:'+CONVERT(varchar(5),ML.Ancho)+' - DIAM:'+CONVERT(varchar(5),ML.Diametro)+' - PERF:'+CONVERT(varchar(5),ML.Perfil)" +
-                            "+' - MmCc:'+CONVERT(varchar(5),ML.MmCocada)+' - Compatible con '+V.MarcaVehiculo+' '+V.ModeloVehiculo as 'Descripcion', LS.Precio, LS.Total, " +
-                            "P.NombresCliente+' '+P.ApellidosCliente, P.FechaActual, P.FechaEntrega, P.DNI, P.Correo from T_Listado LS inner join T_Llanta LL on LS.LlantaId = LL.LlantaId " +
-                            "inner join T_DetalleLlanta DL on DL.DetalleLlantaId = LL.DetalleLlantaId inner join T_Vehiculo V on V.VehiculoId = LL.VehiculoId " +
-                            "inner join T_MedidaLlanta ML on ML.MedidaLlantaId = DL.MedidaLlantaId inner join T_Pedido P on P.codPedido = LS.codPedido where LS.codPedido = '"+codPedido+"';");
+                Map<String,String> insertar = new HashMap<>();
+                insertar.put("codPedido",codPedido);
+                JSONObject datosJSON = new JSONObject(insertar);
 
-                    if (!rs.next()) {
-                        Toast.makeText(getApplicationContext(),"No se encontraron registros",Toast.LENGTH_SHORT).show();
-                    }else {
-                        do {
-                            ItemId = rs.getString(1);
-                            LlantaId = rs.getString(2);
-                            Cantidad = ""+rs.getInt(3);
-                            Descripcion = rs.getString(4);
-                            Precio = ""+rs.getDouble(5);
-                            Total = ""+rs.getDouble(6);
-                            setNeumaticos(ItemId,LlantaId,Cantidad, Descripcion, Precio, Total);
+                AndroidNetworking.post("https://whispering-sea-93962.herokuapp.com/Catalogo_POST_BUSQUEDA_WHERE.php")
+                        .addJSONObjectBody(datosJSON)
+                        .setPriority(Priority.IMMEDIATE)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
 
-                            NombresCliente = rs.getString(7);
-                            FechaActual = rs.getString(8);
-                            FechaEntrega = rs.getString(9);
-                            DNI = ""+rs.getInt(10);
-                            Correo = rs.getString(11);
-                        } while (rs.next());///va agregando cada ID
-                    }
-                    rs.close();
+                                try {
+                                    String validarDatos = response.getString("data");
+                                    int contar = 1;
 
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-                }//FIN SELECT--------------------------------------------------------------------------
+                                    if (validarDatos.equals("[]")) {
+                                        Toast.makeText(getApplicationContext(), "No se encontraron productos", Toast.LENGTH_SHORT).show();
+                                    } else {
+
+                                        JSONArray array = response.getJSONArray("data");
+                                        do {
+                                            JSONObject object = array.getJSONObject(contar - 1);
+
+                                            ItemId = object.getString("itemid");
+                                            LlantaId = object.getString("llantaid");
+                                            Cantidad = ""+object.getInt("cantidad");
+                                            Descripcion = object.getString("descripcion");
+                                            Precio = object.getString("precio");
+                                            Total = object.getString("total");
+                                            setNeumaticos(ItemId,LlantaId,Cantidad, Descripcion, Precio, Total);
+
+                                            NombresCliente = object.getString("nombrescliente");
+                                            FechaActual = object.getString("fechaactual");
+                                            FechaEntrega = object.getString("fechaentrega");
+                                            DNI = ""+object.getString("dni");
+                                            Correo = object.getString("correo");
+
+                                            contar++;
+                                        } while (contar <= array.length());
+
+                                    }
+                                } catch (JSONException e) {
+                                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+
+                            }
+
+                        });//FIN SELECT--------------------------------------------------------------------------
 
 
                 templatePDF.AddTituloEmpresa("SERVIMAQ S.A.C.", "GUIA DE REMISION","CODIGO DE PEDIDO: ",codPedido ,"20455986835");
